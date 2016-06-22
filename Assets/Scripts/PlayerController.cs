92dee03e84fc332bec696a121266f6411231a768
BerniceChua <Bernice.Chua.415@gmail.com>
2016-06-22 16:32:22 -0700

    Add code for collision detection.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,13 +13,15 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertial = Input.GetAxis ("Vertical");
+		float moveVertial = Input.GetAxis("Vertical");
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertial);
 		rb.AddForce (movement * speed);
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Destroy(other.gameObject);
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive(false);
+		}
 	}
 }
