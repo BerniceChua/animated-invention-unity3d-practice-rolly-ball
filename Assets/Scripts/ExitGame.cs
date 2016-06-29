b97e6d66bdb87a914deac35895dbd2581cebaae9
BerniceChua <Bernice.Chua.415@gmail.com>
2016-06-28 23:14:02 -0700

    Add start menu button to exit game.

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExitGame : MonoBehaviour {
+	
+	public Button exit;
+
+	// Use this for initialization
+	public void Start() {
+		exit.onClick.AddListener (() => {
+			Application.Quit();
+		});
+	}
+}
