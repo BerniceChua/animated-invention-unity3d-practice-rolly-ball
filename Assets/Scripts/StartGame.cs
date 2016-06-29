652e230d655c58fb25a67e8afc0877f7716d5f52
BerniceChua <Bernice.Chua.415@gmail.com>
2016-06-29 11:27:16 -0700

    Create Start Menu.

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour {
+	
+	public Button play;
+
+	// Use this for initialization
+	public void Start() {
+		play.onClick.AddListener (() => {
+			this.gameObject.SetActive(false);
+		});
+	}
+}
